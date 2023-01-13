2e5a7ef755cd4e2a73dcfb31ac0857b13a523bc4
omar zairi <zairiomar044@gmail.com>
2023-01-13 16:08:07 +0100

    f

--- main.py
+++ main.py
@@ -1,12 +1,105 @@
-import nltk
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 import warnings
 from flask import Flask, render_template, request
+warnings.filterwarnings('ignore')
+jobs1=pd.read_csv("./input/dataset/jobz.csv")
+jobs2=pd.read_csv("./input/dataset/Modified.csv")
+jobs3=pd.read_csv("./input/dataset/jobss.csv")
+jobs2.rename(columns = {'Title':'jobtitle'}, inplace = True)
+jobs2.rename(columns = {'RequiredQual':'skills'}, inplace = True)
+
+df= pd.concat([jobs1,jobs2,jobs3])
+to_drop = ['JobDescription','JobRequirment','Combined']
+df.drop(to_drop, inplace=True, axis=1)
+
+classes = df['jobtitle'].value_counts()[:200]
+keys = classes.keys().to_list()
+
+df =df[df['jobtitle'].isin(keys)]
+df['jobtitle'].value_counts()
 
 
+def chane_titles(x):
+    x = x.strip()
+    if x == 'Senior Java Developer':
+        return 'Java Developer'
+    elif x == 'Sr Java Developer':
+        return 'Java Developer'
+    elif x == 'Sr. Java Developer':
+        return 'Java Developer'
+    elif x == 'Senior Software Engineer':
+        return 'Software Engineer'
+    elif x == 'Senior QA Engineer':
+        return 'Software QA Engineer'
+    elif x == 'Senior Software Developer':
+        return 'Senior Web Developer'
+    elif x == 'Senior PHP Developer':
+        return 'PHP Developer'
+    elif x == 'Senior .NET Developer':
+        return '.NET Developer'
+    elif x == 'Sr .NET Developer':
+        return '.NET Developer'
+    elif x == 'Sr. .NET Developer':
+        return '.NET Developer'
+    elif x == '.Net Developer':
+        return '.NET Developer'
+    elif x == 'Senior Web Developer':
+        return 'Web Developer'
+    elif x == 'Database Administrator':
+        return 'Database Admin/Dev'
+    elif x == 'Database Developer':
+        return 'Database Admin/Dev'
 
+    else:
+        return x
+
+
+df['jobtitle'] = df['jobtitle'].apply(chane_titles)
+df['jobtitle'].value_counts()
+qty=df['jobtitle'].value_counts()[:5].tolist()
+label=df['jobtitle'].value_counts().index.tolist()
+
+stopwordsSkills=[]
+sk=df["skills"]
+for word in sk:
+    word=str(word)
+    word.lower()
+    word=word.split(',')
+    if(word[0]!=''):
+        stopwordsSkills.append(word[0])
+sdf=pd.DataFrame({'skills':stopwordsSkills})
+qtySkills=sdf["skills"].value_counts().tolist()
+labelSkills=sdf['skills'].value_counts().index.tolist()
+vectorizer=TfidfVectorizer()
+X=vectorizer.fit_transform(df['jobtitle'].values)
+analyze=vectorizer.build_analyzer()
+
+#training the model
+jobSkills=[]
+for i in sdf["skills"]:
+    jobSkills.append(i.lower())
+Xclass=vectorizer.fit_transform(jobSkills)
+X_train,X_test,Y_train,Y_test=train_test_split(Xclass,df['jobtitle'],test_size=0.2,random_state=42)
+#predictions
+
+from sklearn.metrics import classification_report
+from sklearn.svm import SVC
+
+svm=SVC(C=50,gamma=1,kernel='rbf',probability=True)
+svmfit=svm.fit(X_train,Y_train)
+svm_predictions=svmfit.predict(X_test)
+classification_report(Y_test,svm_predictions)
+
+def getJob(sk):
+    skills=''
+    for i in sk:
+        i=i.lower()
+        skills=skills+i+','
+    pred = vectorizer.transform([skills])
+    output = svm.predict(pred)
+    return output[0]
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -15,3 +108,7 @@ app.config['DEBUG'] = True
 def home():
     return "hello"
 #return the getJob function in jason with skills given in body
+@app.route('/getJob', methods=['POST'])
+def getJobb():
+    sk = request.get_json()["skills"]
+    return getJob(sk)
